3eaebd8e6433d1b46875d98d4bd19c419ce33016
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-11-11 16:35:18 -0500

    The connection string is created

--- ShoppingStoreDlloSat/Program.cs
+++ ShoppingStoreDlloSat/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingStoreDlloSat.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Con esta línea ya puedo crear mi BD y usar la instcia de SQL que tengo aquí en mi VisualStudio
+builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
