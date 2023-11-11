17a2f26edaf1a3c108f5bd3bc14ae7a4505baf26
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-11-11 15:41:43 -0500

    the DAL folder is created and the Entity and Tickets classes are added, DataBaseContext is created to create the connection and the DB

---
+++ ShoppingStoreDlloSat/DAL/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingStoreDlloSat.DAL.Entities;
+
+namespace ShoppingStoreDlloSat.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        //En este constructor, creo la refrencia de DbContextOptions que me sirve para configurar las opciones de la BD, como por ejemplo usar SQL Server y usar la cadena de conexión a la BD
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        //DbSet me sirve para convertir mi clase Country en una tabla de BD. El nombre de la tabla será "Countries"
+        public DbSet<Country> Countries { get; set; }
+       
+    }
+}
